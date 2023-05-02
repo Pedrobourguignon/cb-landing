588a3e57863835f48baab9eb7127c5b8a2c5e3e4
Pedrobourguignon <pedrobourguignon@hotmail.com>
2023-05-02 18:47:29 -0300

    feat: add fb pixel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import router from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    import("react-facebook-pixel")
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init("974514130400605");
+        ReactPixel.pageView();
+
+        router.events.on("routeChangeComplete", () => {
+          ReactPixel.pageView();
+        });
+      });
+  }, [router.events]);
   return (
     <>
       <Head>
