f56eaff6f140c5e36bb7df02f052dc61c7b77129
Pedrobourguignon <pedrobourguignon@hotmail.com>
2023-05-02 19:04:47 -0300

    feat: add pixel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import router from "next/router";
-import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    import("react-facebook-pixel")
-      .then((x) => x.default)
-      .then((ReactPixel) => {
-        ReactPixel.init("974514130400605");
-        ReactPixel.pageView();
-
-        router.events.on("routeChangeComplete", () => {
-          ReactPixel.pageView();
-        });
-      });
-  }, [router.events]);
   return (
     <>
       <Head>
         {/* Primary Meta Tags */}
+        
         <title>CB Beauty</title>
         <meta name="title" content="CB Beauty" />
         <link rel="icon" href="/logotipo.jpg" />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,11 @@
+import FacebookPixel from "@/utils/facebookPixel";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <FacebookPixel />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
