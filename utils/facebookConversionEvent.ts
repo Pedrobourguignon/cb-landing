067f02c91599ac35d8aa169165cfda35b5200ff3
Pedrobourguignon <pedrobourguignon@hotmail.com>
2023-05-02 19:47:56 -0300

    fix: dotenv

--- utils/facebookConversionEvent.ts
+++ utils/facebookConversionEvent.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const sendEvent = async (eventData: Record<string, any>) => {
-  const { FACEBOOK_AD_ACCOUNT_ID, FACEBOOK_ACCESS_TOKEN } = process.env;
-
-  const apiUrl = `https://graph.facebook.com/v12.0/${FACEBOOK_AD_ACCOUNT_ID}/events?access_token=${FACEBOOK_ACCESS_TOKEN}`;
+  const apiUrl = `https://graph.facebook.com/v12.0/${process.env.NEXT_PUBLIC_FACEBOOK_AD_ACCOUNT_ID}/events?access_token=${process.env.NEXT_PUBLIC_FACEBOOK_ACCESS_TOKEN}`;
 
   try {
     const response = await axios.post(apiUrl, eventData);
