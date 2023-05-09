0f84f9846daeec66c525835484eb7e6e2826886e
Pedrobourguignon <pedrobourguignon@hotmail.com>
2023-05-09 12:01:59 -0300

    feat: back purchase event

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -1,24 +1,24 @@
 import { usePicasso } from "@/hooks/usePicasso";
+import sendEvent from "@/utils/facebookConversionEvent";
 // import sendEvent from "@/utils/facebookConversionEvent";
-import { Button, FlexProps, Link } from "@chakra-ui/react";
-import NextLink from "next/link";
+import { Button, FlexProps } from "@chakra-ui/react";
 
 export const PrimaryButton: React.FC<FlexProps> = ({ height }) => {
   const theme = usePicasso();
-  // const handleConversion = () => {
-  //   sendEvent({
-  //     event_name: "Purchase",
-  //     event_time: Math.floor(Date.now() / 1000),
-  //     user_data: {
-  //       email: "john.doe@example.com",
-  //     },
-  //     custom_data: {
-  //       currency: "USD",
-  //       value: "100.00",
-  //     },
-  //   });
-  //   window.open("https://pay.kiwify.com.br/5vk05Tc");
-  // };
+  const handleConversion = () => {
+    sendEvent({
+      event_name: "Purchase",
+      event_time: Math.floor(Date.now() / 1000),
+      user_data: {
+        email: "john.doe@example.com",
+      },
+      custom_data: {
+        currency: "USD",
+        value: "100.00",
+      },
+    });
+    window.open("https://pay.kiwify.com.br/5vk05Tc");
+  };
 
   return (
     <Button
@@ -30,7 +30,7 @@ export const PrimaryButton: React.FC<FlexProps> = ({ height }) => {
       w="60"
       boxShadow="xl"
       _hover={{}}
-      // onClick={() => handleConversion()}
+      onClick={() => handleConversion()}
       zIndex="999"
     >
       QUERO RECEBER
