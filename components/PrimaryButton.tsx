823cfe2b67d399709dd5c385be8290125f6511ac
Pedrobourguignon <pedrobourguignon@hotmail.com>
2023-05-09 12:34:03 -0300

    feat: try with library

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -1,11 +1,17 @@
 import { usePicasso } from "@/hooks/usePicasso";
+import { fbEvent } from "@rivercode/facebook-conversion-api-nextjs";
 // import sendEvent from "@/utils/facebookConversionEvent";
 import { Button, FlexProps } from "@chakra-ui/react";
 
 export const PrimaryButton: React.FC<FlexProps> = ({ height }) => {
   const theme = usePicasso();
   const handleConversion = () => {
-    fbq("track", "Purchase", { currency: "USD", value: 30.0 });
+    fbEvent({
+      eventName: "Purchase",
+      value: 10,
+      currency: "BRL",
+    });
+
     window.open("https://pay.kiwify.com.br/5vk05Tc");
   };
 
@@ -19,7 +25,7 @@ export const PrimaryButton: React.FC<FlexProps> = ({ height }) => {
       w="60"
       boxShadow="xl"
       _hover={{}}
-      onClick={() => handleConversion()}
+      onClick={handleConversion}
       zIndex="999"
     >
       QUERO RECEBER
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,27 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import {
+  FBPixelScript,
+  FBPixelProvider,
+} from "@rivercode/facebook-conversion-api-nextjs/components";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         {/* Primary Meta Tags */}
-        
+
         <title>CB Beauty</title>
         <meta name="title" content="CB Beauty" />
         <link rel="icon" href="/logotipo.jpg" />
       </Head>
-      <ChakraProvider>
-        <Component {...pageProps} />
-      </ChakraProvider>
+      <FBPixelScript />
+      <FBPixelProvider>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </FBPixelProvider>
     </>
   );
 }
